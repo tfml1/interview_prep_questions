450581016293805171f32f8b24c5dcf454410b62
html_css_js_react_rtk <143926881+tfml1@users.noreply.github.com>
2023-10-08 19:33:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import FunctCompTest from "./components/FunctCompTest";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FunctCompTest />
+      {/* ------------------------------------------------------------------------------------------------- */}
+      check out notes txt files in src
+      {/* ------------------------------------------------------------------------------------------------- */}
+    </div>
+  );
+}
---
+++ src/components/FunctCompTest.js
@@ -0,0 +1,18 @@
+// import "./abcdxyz123.css";
+import React from "react";
+
+export default function FunctCompTest() {
+  return (
+    <div className="App">
+      <p
+        style={
+          {
+            /* u can do some inline styling here or give it a clsnm/id */
+          }
+        }
+      >
+        FnctComp
+      </p>
+    </div>
+  );
+}
